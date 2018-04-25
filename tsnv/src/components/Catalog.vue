888195b41f3be8b6add83c52bf57d4ce0927165e
Rin Ternone <gifgaf@gmail.com>
2018-04-25 16:54:50 +0300

    catalog

--- tsnv/src/components/Catalog.vue
+++ tsnv/src/components/Catalog.vue
@@ -1,46 +1,38 @@
 <template>
   <v-layout wrap>
-    <v-flex xs12 text-xs-center>
-       <v-btn 
-       v-for="item in abc" 
-       :key="item" 
-       @click="" fab small light color="secondary" class='mx-0 my-0 noupper'>
+    <v-flex xs12 text-xs-center mb-2>
+       <v-btn
+       fab small light color="secondary" class='mx-0 my-0 noupper'
+       v-for="item in abc"
+       :key="item"
+       @click="">
          {{ item }}
        </v-btn>
     </v-flex>
 
-    <v-flex xs12 sm6 lg3>
-    <v-card class="mb-2 mr-1">
-      <v-card-media
-        src="/static/catalog/vimpels.jpg"
-        height="200px"
-      >
-      </v-card-media>
-      <v-card-title primary-title>
-        <div>
-          <div class="headline">Вымпелы</div>
-          <span class="grey--text">Атласный твердый с прокладкой ламинированный мягкий</span>
-        </div>
-      </v-card-title>
-      <v-card-actions>
-        <v-btn icon flat color="primary">
-          <v-icon>share</v-icon>
-        </v-btn>
-        <v-spacer></v-spacer>
-        <v-btn flat color="primary">Подробнее</v-btn>
-        <v-btn flat color="primary" icon @click.native="show = !show">
-          <v-icon>{{ show ? 'keyboard_arrow_up' : 'keyboard_arrow_down' }}</v-icon>
-        </v-btn>
-      </v-card-actions>
-      <v-slide-y-transition>
-        <v-card-text v-show="show">
-          Мы имеем собственное производство в СПб по изготовлению вымпелов, где используются все современные технологии. дает   возможность изготовить вымпелы на заказ четырех различных видов для всех случаев, когда нужно подарить на памсимпатичный  сувенир, наградить победителей спортивных и производственных соревнований или отметить участников важнсобытия.
-        </v-card-text>
-      </v-slide-y-transition>
-    </v-card>
+    <v-flex xs12 sm6 lg3
+      v-for="item in cards" 
+      :key="item.title"
+    >
+      <v-card class="">
+        <v-card-media
+          :src="item.picsrc"
+          height="240px"
+        >
+        </v-card-media>
+        <v-card-title primary-title text-xs-centerclass="">
+            <div class="headline"> {{ item.title }} </div>
+        </v-card-title>
+        <p class="grey--text"> {{ item.description }} </p>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn icon class="noupper primary--text">
+            <v-icon>arrow_forward</v-icon>
+          </v-btn>
+        </v-card-actions>
+      </v-card>
     </v-flex>
-    
-  </v-layout>  
+  </v-layout>
 </template>
 
 <script>
@@ -49,13 +41,42 @@
       show: false,
       abc: [
         'Все', 'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ж', 'З', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Ц', 'Ш', 'Э'],
-      partItems: [
-        { icon: '', title: 'Вымпелы', description: 'Печать фирменных, государственных, ведомственных, спортивных и других флагов и знамен любого размера.' }
+      cards: [
+        {title: 'Банданы', description: 'Разные-разные, голубые, красные', picsrc: '/static/catalog/bandanas.jpg'
+        },
+        {title: 'Баннеры', description: 'Ну очень большие, но по пять', picsrc: '/static/catalog/banners.jpg'
+        },
+        {title: 'Бейсболки', description: 'Разные-разные, голубые, красные', picsrc: '/static/catalog/baseballcap.jpg'
+        },
+        {title: 'Вымпелы', description: 'Разные-разные, голубые, красные', picsrc: '/static/catalog/vimpels.jpg'
+        },
+        {title: 'Флаги', description: 'Разные-разные, голубые, красные', picsrc: '/static/catalog/flags.jpg'
+        },
+        {title: 'Флажки на палочке', description: 'Разные-разные, голубые, красные', picsrc: '/static/catalog/stickflags.jpg'
+        },
+        {title: 'Флагштоки', description: 'Разные-разные, голубые, красные', picsrc: '/static/catalog/flagpoles.jpg'
+        },
+        {title: 'Фотообои', description: 'Разные-разные, голубые, красные', picsrc: '/static/catalog/wallpapers.jpg'
+        },
+        {title: 'Фотошторы', description: 'Разные-разные, голубые, красные', picsrc: '/static/catalog/curtains.jpg'
+        },
+        {title: 'Футболки с логотипом', description: 'Разные-разные, голубые, красные, жёлтые, зелёные', picsrc: '/static/catalog/tshirts.jpg'
+        }
       ]
     })
   }
 </script>
 
 <style scoped>
-  .mx-145{margin: 0}
+.container.grid-list-lg .layout .flex{
+  padding: 5px;
+}
+.card{
+  padding: 5px;
+}
+.headline {
+    font-size: 18px!important;
+    font-weight: 600;
+    line-height: 18px!important;
+}    
 </style>
